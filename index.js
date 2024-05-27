a9065a8e044a23d38d352384a5c5bf5fa9b77c34
zalogg <gonzaloguachamin2330@gmail.com>
2024-05-27 12:12:10 -0500

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs'); 
 app.use(express.static('public')); 
